48ce33f17686c74757bc697e04dbf15f773e27e1
iamhvsharma <itshvsharma@gmail.com>
2024-05-15 23:46:06 +0530

    features

---
+++ folder1/features.js
@@ -0,0 +1,13 @@
+const gfName = "MrsRandom";
+
+// This is when you have only one thing to export
+export default gfName; // This allows you to export this module or code and import in any other file or folder
+
+const gfName2 = "MrsRandom2";
+const gfName3 = "MrsRandom3";
+
+// if we have more than one thing to export then we will follow the below syntax
+
+export {gfName2, gfName3};
+
+
